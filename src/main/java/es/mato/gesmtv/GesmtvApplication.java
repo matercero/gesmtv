b3b0c6f82e277e877cca23e576683064ccbc8fa2
Miguel III <matercero@gmail.com>
2021-09-08 08:27:13 +0200

    1 version

---
+++ src/main/java/es/mato/gesmtv/GesmtvApplication.java
@@ -0,0 +1,13 @@
+package es.mato.gesmtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesmtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesmtvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mato/gesmtv/GesmtvApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mato.gesmtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesmtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
